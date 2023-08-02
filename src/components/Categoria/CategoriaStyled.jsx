45e04b702223d2cfb838f24f3886dbb751ea874a
Josué Ramos <niallswiftnerd@outlook.com>
2023-08-02 01:26:16 -0300

    attrs

---
+++ src/components/Categoria/CategoriaStyled.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+
+export const Content = styled.section`
+    width: 100%;
+    height: 15vh;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    overflow-x:scroll;
+
+
+
+::-webkit-scrollbar{
+    display: none;
+}
+`
+export const Button = styled.button`
+    width: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 15px;
+    border-radius: 12px;
+    background-color: var(--color-white);
+    border: none;
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+    padding: 5px 15px;
+    font-family: 'PoppinsRegular', 'Rubik', sans-serif;
+    color: var(--color-black)!important;
+    font-size: 20px;
+
+    
+    :hover,:active{
+        background: var(--color-primary);
+    }
+    svg{
+        font-size: 2rem;
+        margin-right: 5px;
+        color: var(--color-black)!important;
+    }
+`
---
+++ src/components/Categoria/index.jsx
@@ -0,0 +1,20 @@
+import { Content, Button} from "./CategoriaStyled"
+/////icons/////////
+import { LuIceCream2, LuSalad, LuWine} from 'react-icons/lu';
+//////////////////
+////// Clobal context //
+import {ContextGlobal} from '../../contexts/auth'
+import { useContext } from "react";
+///////////////////////////////////
+
+export default function Categoria(){
+    const {SetCategoria} = useContext(ContextGlobal);
+
+    return(
+        <Content>
+            <Button onClick={() => SetCategoria("Refeição")}> <LuSalad/> Refeições </Button>
+            <Button onClick={() => SetCategoria("Berbida")}> <LuWine/> Berbidas</Button>
+            <Button onClick={() => SetCategoria("Sobremesa")}> <LuIceCream2/> Sobremesas</Button>
+        </Content>
+    )
+}
