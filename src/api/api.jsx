f64f431bfbb76f3a9baa101b8bb9550ce767aeed
niall swift <niallswift14@gmail.com>
2024-02-24 11:47:30 -0300

    list img product

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -5,7 +5,7 @@ const UseAPIClient = (ctx) => {
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: 'http://localhost:3333',
+        baseURL: 'https://back-end-dayfood-pkjip5np4-niall-swift.vercel.app/',
         headers: {
             Authorization: `Bearer ${cookies['@dayfood.token']}`
         }
